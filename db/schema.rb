373d50599addd6c9526425cad900605a62a26ba3
Johannes Boehmer <j.boehmer@live.de>
2025-01-31 17:49:30 +0100

    first step in character-show and model adaptations

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2025_01_28_133014) do
+ActiveRecord::Schema[7.1].define(version: 2025_01_31_163212) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -57,6 +57,9 @@ ActiveRecord::Schema[7.1].define(version: 2025_01_28_133014) do
     t.bigint "user_id", null: false
     t.bigint "game_id", null: false
     t.boolean "alive", default: true
+    t.string "race"
+    t.string "alignment"
+    t.string "background"
     t.index ["game_id"], name: "index_characters_on_game_id"
     t.index ["user_id"], name: "index_characters_on_user_id"
   end
